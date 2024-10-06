6ab5f67706a090028aade1ede85ca29f44ddd674
kuntal14 <sumankuntal6@gmail.com>
2024-10-06 18:21:20 +0530

    this is the correct file lol

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home">
+    <h1>DropZone</h1>
+    <DropZone @drop.prevent="drop" @change="selectedFile" />
+    <span class="file-info">File : {{ localFile.name }} </span>
+  </div>
+ 
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+import { ref } from 'vue';
+import DropZone from './components/DropZone.vue'
+
+let localFile = ref("")
+
+const drop = (e) => {
+  localFile.value = e.dataTransfer.files[0]
 }
+
+const selectedFile = () => {
+  localFile.value = document.querySelector('.localFile').files[0]
+}
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.home{
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #f1f1f1;
+
+  h1{
+    font-size: 40px;
+    margin-bottom: 32px;
+  }
+
+  .file-info{
+    margin-top: 32px;
+  }
 }
+
+
 </style>
---
+++ src/components/DropZone.vue
@@ -0,0 +1,67 @@
+<template>
+  <div 
+    @dragenter.prevent="toggleActive" 
+    @dragleave.prevent="toggleActive" 
+    @dragover.prevent
+    @drop.prevent="toggleActive"
+    :class="{'active-dropzone': active}"
+    class="dropzone"
+  >
+    <span>Drag or drop files</span>
+    <span>OR</span>
+    <label for="localFile">Select file</label>
+    <input type="file" id="localFile" class="localFile" />
+  </div>
+</template>
+
+<script setup>
+
+  import { ref } from 'vue';
+
+  const active = ref(false);
+
+  const toggleActive = () => {
+    active.value = !active.value;
+    console.log(active.value)
+  };
+
+</script>
+
+
+<style scoped>
+.dropzone {
+  width: 400px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 16px;
+  border: 2px dashed #41b883;
+  background-color: #fff;
+  transition: 0.3s ease all;
+
+  label{
+    padding: 8px 12px;
+    color: #fff;
+    background-color: #41b883;
+    transition: 0.3s ease all;
+  }
+
+  input{
+    display: none;
+  }
+}
+
+.active-dropzone{
+  color: #fff;
+  border-color: #fff;
+  background-color: #41b883;
+
+  label{
+    background-color: #fff;
+    color: #41b883;
+  }
+}
+
+</style>
